6e21ab7073c609fbd98ae337a609798c58054770
monsalex23 <monsalex23@gmail.com>
2022-06-30 13:54:01 -0300

    learning operators and data types in java

---
+++ src/br/com/dio/Main.java
@@ -0,0 +1,57 @@
+package br.com.dio;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        byte b1 = 10;
+        byte b2 = 20;
+
+        short s1 = 20000;
+        //short s2 = 40000; tamanho pasa o maximo do tipo de dato
+
+        //int i1 = -10000000000; exede o limite inferior do un entero
+        int i2 = 28500;
+
+        long l1 = 1000000000000000000L;
+        long l2 = 2004005000500055000L;
+
+        //float f1 = 4.5; e un double
+        float f2 = 10.68F;
+
+        double d1 = 85.69;
+        double d2 = 99.04D;
+
+        char c1 = 'W';
+        //char c2 = 'Tw'; char e so um caractere
+        char c3 = '\u0057';
+
+        String st1 = "Fulano";
+        String st2 = "Cicrano";
+        String st3 = "ag dgb se  se et t KNBJBJBB &*¨&%& 75894389";
+
+        //String dt1 = "09/02/1981";  a data tem um termino especifico para describir e pode ser comparado
+
+        boolean bo1 = true;
+        boolean bo2 = false;
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(s1);
+        System.out.println(i2);
+        System.out.println(b1);
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(f2);
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(c1);
+        System.out.println(c3);
+        System.out.println(st1);
+        System.out.println(st2);
+        System.out.println(st3);
+        System.out.println(bo1);
+        System.out.println(bo2);
+
+    }
+}
